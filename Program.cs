784bb697af7f238d3e1a9cee1c55612b5c8e56cf
NoraKitchen <narwhaltortellini@gmail.com>
2016-09-13 00:01:08 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //prompt user for minutes exercised
+        System.Console.Write("Enter how many minues you exercised: ");
+        //add minutes exercised to total
+        //display total min exercised to screen
+        //repeat until user quits
+    }
+}
